69396a1f8b117f4664eff3305b5c8d899a6a8da3
Archana-97 <archanakm82@gmail.com>
2025-04-05 20:47:08 +0530

    Add files via upload
    Week 2 -Day1

---
+++ day1/Library.java
@@ -0,0 +1,20 @@
+package week2.day1;
+
+public class Library {
+	public String addBook(String bookTitle) {
+		System.out.println("Book added successfully");
+		return bookTitle;
+	}
+	public void issueBook() {
+		System.out.println("Book issued successfully");
+	}
+
+	public static void main(String[] args) {
+		
+		Library bookStatus = new Library();
+		bookStatus.addBook("Book");
+		bookStatus.issueBook();	
+		
+	}
+
+}
